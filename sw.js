94e899730e2ea3e925e12f6e24756b48a645afd8
Whien <sal95610@gmail.com>
2018-10-18 15:47:46 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-92c4aec54e231bdca37e.js"
+    "url": "webpack-runtime-96e8f71c264c9b692acc.js"
   },
   {
     "url": "app-24e83b9aed8fb0efca27.js"
@@ -36,17 +36,17 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "d9026b62a54b12d9ac4c8eac56112ae9"
+    "revision": "ddacce2c58de807534a100bc21c89e7e"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3966ca72b71da9c5a33f16b8cfa5b6e6"
+    "revision": "33bc34169f2292500170623e745a67b2"
   },
   {
     "url": "component---src-pages-index-js.43bb6d5b0784359491aa.css"
   },
   {
-    "url": "component---src-pages-index-js-909a629a6b6cf70e32d1.js"
+    "url": "component---src-pages-index-js-c1fd028e46348aa2107d.js"
   },
   {
     "url": "0-223ffa3b452b771ac3f9.js"
